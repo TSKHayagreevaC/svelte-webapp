b1063ea5baf8f2cb216baa12bb4be3040dbe2fc0
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2023-11-14 10:25:13 +0530

    feat: Added 'blog' route.

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<section>
+	<h1>Blog: {$page.params.slug}</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
